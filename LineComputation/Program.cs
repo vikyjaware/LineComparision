1a2694a69beb4f062ac0ce86da9fb70011c839a7
vikyjaware <vikyjaware@gmail.com>
2022-01-29 08:23:24 +0530

    [Viky] Add : Folder structure added

---
+++ LineComputation/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LineComputation;
+
+Console.WriteLine("Welcome to Line Comparison Computation Program");
+
+LineComparison Lengthcal = new LineComparison();
+Lengthcal.CalculateLength();
